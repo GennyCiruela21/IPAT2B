8341fa1e6e5a209d0419bbb01cd32761c0c9a70c
GennyCiruela21 <ciruela.g.bsinfotech@gmail.com>
2024-05-30 12:02:36 +0800

    records

--- CIRUELA/settings.py
+++ CIRUELA/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'CIRUELA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gipat2b',
+        'USER': 'root' ,
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306', 
     }
 }
 
